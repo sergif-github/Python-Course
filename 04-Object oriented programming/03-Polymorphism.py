c8a53f1e5472a894a053580a6b3850d1b0688868
Sergi Felip Ribas <sergifelip96@gmail.com>
2023-03-06 10:18:11 +0100

    Added Object orientated programming

---
+++ 04-Object oriented programming/03-Polymorphism.py
@@ -0,0 +1,45 @@
+# Polymorphism
+print('''Polymorphism''')
+'''Polymorphism refers to the way in which different object classes can share the same method name,
+ and those methods can be called from the same place even though a variety of different objects might be passed in'''
+
+class Cat:
+    def __init__(self, name):
+        self.name = name
+
+    def speak(self):
+        return self.name+' says Meow!'
+
+
+c = Cat()
+print(d.speak())
+print(c.speak())
+
+
+#An abstract class is one that never expects to be instantiated
+
+class Animal:
+    def __init__(self, name):  # Constructor of the class
+        self.name = name
+
+    def speak(self):  # Abstract method, defined by convention only
+        raise NotImplementedError("Subclass must implement abstract method")
+
+
+class Dog(Animal):
+
+    def speak(self):
+        return self.name + ' says Woof!'
+
+
+class Cat(Animal):
+
+    def speak(self):
+        return self.name + ' says Meow!'
+
+
+dog = Dog('Fido')
+cat = Cat('Isis')
+
+print(dog.speak())
+print(cat.speak())
---
+++ 05-Modules and packages/01-Modules and packages.py
@@ -0,0 +1,5 @@
+# Modules and packages
+print('''Modules and packages''')
+
+print('''Modules in Python are simply Python files with the .py extension, which implement a set of functions''')
+# To import a module, we use the import command
