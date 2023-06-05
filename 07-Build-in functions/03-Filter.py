c3f3d76ee5bec914fd0fb0a2be5b02ef52679911
sergi <sergifelip96@gmail.com>
2023-06-05 19:28:42 +0200

    deleting files

--- 07-Build-in functions/03-Filter.py
+++
@@ -1,9 +0,0 @@
-# FILTER
-print('''FILTER''')
-
-print('''Filter() filter out all the elements of an iterable, for which the function returns True.''')
-
-lst = list(range(20))
-print("My list: ", lst)
-print("Even values in my list: ", list(filter(lambda x: x % 2 == 0, lst)))
-print("Odd values in my list: ", list(filter(lambda x: x % 2 != 0, lst)))
--- 07-Build-in functions/07-Complex.py
+++
@@ -1,9 +0,0 @@
-# COMPLEX
-print('''COMPLEX''')
-
-print('''Complex() returns a complex number or converts a string or number to a complex.''')
-
-print("Complex(2,3):", complex(2, 3))
-print("Complex(5):", complex(2))
-
-print("Complex('12+2j'):", complex('12+2j'))
